7c1fbc0702070065ae7bb323c7cba7be57d0b4b7
1912820899 <779931346@qq.com>
2021-05-31 18:07:47 +0800

    测试 keyof

--- Omit.ts
+++ Omit.ts
@@ -6,7 +6,13 @@ interface Todo {
   description: string;
   completed: boolean;
 }
-
+type a = keyof Todo;
+type b = "title" | "description" | "completed";
+let aa: a;
+let bb: b;
+aa = bb;
+aa = "completed";
+bb = "description";
 type TodoPreview = myOmit<Todo, "description" | "title">;
 
 const todo: TodoPreview = {
