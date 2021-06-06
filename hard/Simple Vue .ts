310c20175738f77444646805d0f29d54b34c1ae5
1912820899 <779931346@qq.com>
2021-06-06 22:39:56 +0800

    卧槽了，实现不了呢，日

--- hard/Simple Vue .ts
+++ hard/Simple Vue .ts
@@ -1,43 +1,44 @@
-interface IData {
-  (): {
-    [k: string]: any;
-  };
-}
+type TranslateObj<T extends IComputed = IComputed> = {
+  [k in keyof T]: ReturnType<T[k]>;
+};
 
+interface IDataCB {
+  [k: string]: any;
+}
 interface IComputed {
-  [k: string]: () => string;
+  [k: string]: {
+    (): any;
+  };
 }
 
 interface IMethods {
   [k: string]: () => any;
 }
 
-interface Option {
-  data?: IData;
-  computed?: IComputed & ThisType<IData>;
-  methods?: IMethods & ThisType<IData>;
+interface Option<D extends IDataCB, C extends IComputed, M> {
+  data: (this: void) => D;
+  computed: C & ThisType<D>;
+  methods: M & ThisType<D & M>;
 }
 
-interface Instance {
-  [k: string]: () => void;
-}
+interface Instance {}
 
-type SimpleVue = (
-  obj: Option
-) => Instance & ThisType<IComputed> & ThisType<IData>;
+// type SimpleVue<D, C, M> = (obj: Option<D, C, M>) => Instance;
 
-declare const SimpleVue: SimpleVue;
+declare function SimpleVue1<D, CC, M>(options: Option<D, CC, M>): any;
 
-const instance = SimpleVue({
+// const SimpleVue: SimpleVue<IDataCB, IComputed, IMethods> = () => {};
+SimpleVue1({
   data() {
     return {
       firstname: "Type",
       lastname: "Challenges",
-      amount: 10,
+      amount: "10",
     };
   },
   computed: {
     fullname() {
+      // this.
       return this.firstname + " " + this.lastname;
     },
   },
